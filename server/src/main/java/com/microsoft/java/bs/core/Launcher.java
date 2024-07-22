e65319543fe6b19291b503514dd683e3f82d8fc5
Jiaming <71547730+Jiaaming@users.noreply.github.com>
2024-07-22 16:05:55 +0800

    feat - Support launch by using named pipe (#162)

--- server/src/main/java/com/microsoft/java/bs/core/Launcher.java
+++ server/src/main/java/com/microsoft/java/bs/core/Launcher.java
@@ -3,9 +3,14 @@
 
 package com.microsoft.java.bs.core;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.Executors;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 import com.microsoft.java.bs.core.internal.gradle.GradleApiConnector;
 import com.microsoft.java.bs.core.internal.log.LogHandler;
@@ -15,8 +20,10 @@ import com.microsoft.java.bs.core.internal.managers.PreferenceManager;
 import com.microsoft.java.bs.core.internal.server.GradleBuildServer;
 import com.microsoft.java.bs.core.internal.services.BuildTargetService;
 import com.microsoft.java.bs.core.internal.services.LifecycleService;
+import com.microsoft.java.bs.core.internal.transport.NamedPipeStream;
 
 import ch.epfl.scala.bsp4j.BuildClient;
+import org.apache.commons.lang3.StringUtils;
 
 /**
  * Main entry point for the BSP server.
@@ -36,12 +43,35 @@ public class Launcher {
   public static void main(String[] args) {
     checkRequiredProperties();
 
-    org.eclipse.lsp4j.jsonrpc.Launcher<BuildClient> launcher = createLauncher();
+    org.eclipse.lsp4j.jsonrpc.Launcher<BuildClient> launcher;
+    Map<String, String> params = parseArgs(args);
+    String pipePath = params.get("pipe");
+    if (StringUtils.isNotBlank(pipePath)) {
+      launcher = createLauncherUsingPipe(pipePath);
+    } else {
+      launcher = createLauncherUsingStdIo();
+    }
+
     setupLoggers(launcher.getRemoteProxy());
     launcher.startListening();
   }
 
-  private static org.eclipse.lsp4j.jsonrpc.Launcher<BuildClient> createLauncher() {
+  private static org.eclipse.lsp4j.jsonrpc.Launcher<BuildClient> 
+      createLauncherUsingPipe(String pipePath) {
+    NamedPipeStream pipeStream = new NamedPipeStream(pipePath);
+    try {
+      return createLauncher(pipeStream.getOutputStream(), pipeStream.getInputStream());
+    } catch (IOException e) {
+      throw new IllegalStateException("Error initializing the named pipe", e);
+    }
+  }
+
+  private static org.eclipse.lsp4j.jsonrpc.Launcher<BuildClient> createLauncherUsingStdIo() {
+    return createLauncher(System.out, System.in);
+  }
+
+  private static org.eclipse.lsp4j.jsonrpc.Launcher<BuildClient> 
+      createLauncher(OutputStream outputStream, InputStream inputStream) {
     BuildTargetManager buildTargetManager = new BuildTargetManager();
     PreferenceManager preferenceManager = new PreferenceManager();
     GradleApiConnector connector = new GradleApiConnector(preferenceManager);
@@ -50,14 +80,14 @@ public class Launcher {
         connector, preferenceManager);
     GradleBuildServer gradleBuildServer = new GradleBuildServer(lifecycleService,
         buildTargetService);
-    org.eclipse.lsp4j.jsonrpc.Launcher<BuildClient> launcher =
-        new org.eclipse.lsp4j.jsonrpc.Launcher.Builder<BuildClient>()
-          .setOutput(System.out)
-          .setInput(System.in)
-          .setLocalService(gradleBuildServer)
-          .setRemoteInterface(BuildClient.class)
-          .setExecutorService(Executors.newCachedThreadPool())
-          .create();
+    org.eclipse.lsp4j.jsonrpc.Launcher<BuildClient> launcher = new 
+        org.eclipse.lsp4j.jsonrpc.Launcher.Builder<BuildClient>()
+        .setOutput(outputStream)
+        .setInput(inputStream)
+        .setLocalService(gradleBuildServer)
+        .setRemoteInterface(BuildClient.class)
+        .setExecutorService(Executors.newCachedThreadPool())
+        .create();
     buildTargetService.setClient(launcher.getRemoteProxy());
     return launcher;
   }
@@ -79,5 +109,23 @@ public class Launcher {
       telemetryHandler.setLevel(Level.INFO);
       LOGGER.addHandler(telemetryHandler);
     }
+  }
+
+  /**
+   * Parse the arguments and return a map of key-value pairs.
+   */
+  public static Map<String, String> parseArgs(String[] args) {
+    Map<String, String> paramMap = new HashMap<>();
+    for (String arg : args) {
+      if (arg.startsWith("--")) {
+        int index = arg.indexOf('=');
+        if (index != -1) {
+          String key = arg.substring(2, index);
+          String value = arg.substring(index + 1);
+          paramMap.put(key, value);
+        }
+      }
+    }
+    return paramMap;
   }
 }
