a52b1e820991649ffa799b286369be01f34c0e6b
Arthur McGibbon <arthur.mcgibbon@h3im.com>
2025-03-01 14:35:54 +0000

    Setup dependencyUpdates task

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+
 plugins {
   alias(libs.plugins.dependencyUpdates)
   // needed here (even though not applied) to prevent class loader issues
@@ -19,5 +21,33 @@ subprojects {
   checkstyle {
     toolVersion = "10.21.3"
     maxWarnings = 0
+  }
+}
+
+fun isNonStable(version: String): Boolean {
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
+    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+    val isStable = stableKeyword || regex.matches(version)
+    return isStable.not()
+}
+
+tasks.named<DependencyUpdatesTask>("dependencyUpdates").configure {
+  checkConstraints = true
+  //checkBuildEnvironmentConstraints = true
+  checkForGradleUpdate = true
+
+  resolutionStrategy {
+    componentSelection {
+      all {
+        if (candidate.group == "ch.epfl.scala" &&
+            candidate.module == "bsp4j") {
+          if (candidate.version == "2.2.0-M4.TEST") {
+            reject("Invalid release")
+          }
+        } else if (isNonStable(candidate.version)) {
+            reject("Release candidate")
+        }
+      }
+    }
   }
 }
